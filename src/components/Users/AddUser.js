1fec3ad13c8bec62c02234f4493afe619ad9333c
Andrea Cagnetta <andrea.cagnetta@techedgegroup.com>
2021-11-23 10:47:00 +0100

    Add list of Users

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -9,13 +9,16 @@ const AddUser = (props) => {
 
     const addUserHandler = (event) => {
         event.preventDefault();
-        if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+        if (
+            enteredUsername.trim().length === 0 ||
+            enteredAge.trim().length === 0
+        ) {
             return;
         }
         if (enteredAge < 1) {
             return;
         }
-        console.log(enteredUsername, enteredAge);
+        props.onAddUser(enteredUsername, enteredAge);
         setEnteredUsername("");
         setEnteredAge("");
     };
--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,15 +1,19 @@
-import userEvent from "@testing-library/user-event";
-import React from "react";
+import React from 'react';
+import classes from './UsersList.module.css';
+
+import Card from '../UI/Card';
 
 const UsersList = (props) => {
     return (
-        <ul>
-            {props.users.map((user) => (
-                <li>
-                    {user.name} ({user.age} years old)
-                </li>
-            ))}
-        </ul>
+        <Card className={classes.users}>
+            <ul>
+                {props.users.map((user) => (
+                    <li key={user.id}>
+                        {user.name} ({user.age} years old)
+                    </li>
+                ))}
+            </ul>
+        </Card>
     );
 };
 
